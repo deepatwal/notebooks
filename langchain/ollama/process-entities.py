5da0d949546fab81905f78d56f7f2e05fabbcd56
Deepak Patwal <deepatwal@gmail.com>
2025-04-27 21:27:52 +0200

    .

--- langchain/ollama/process-entities.py
+++ langchain/ollama/process-entities.py
@@ -33,8 +33,8 @@ SPARQL_ENDPOINT = urllib.parse.urljoin(GRAPHDB_BASE_URL.rstrip('/') + '/', f"rep
 OUTPUT_FILENAME_DIR = os.path.join("c:\\Users\\deepa\\data\\workspace\\notebooks", "datasets", "cache")
 DB_PATH = os.path.join(OUTPUT_FILENAME_DIR, "cache.db")
 
-MAX_CONCURRENT_REQUESTS = 1
-MAX_CONCURRENT_CLASSES = 1
+MAX_CONCURRENT_REQUESTS = 5
+MAX_CONCURRENT_CLASSES = 5
 BATCH_SIZE = 50
 
 instance_lock = Lock()
